3eec68bd1d769f4b58fe952fade7f25921431ced
Haider ali <uchiha.usama@gmail.com>
2021-04-26 18:41:14 +0300

    fourth commit different approach

--- rna_transcription.dart
+++ rna_transcription.dart
@@ -1,14 +1,23 @@
 class RnaTranscription {
-  Map<String, String> _map = {
-    'G': 'C',
-    'C': 'G',
-    'T': 'A',
-    'A': 'U',
-  };
-
-  String toRna(String code) {
-    String result = '';
-    code.split('').forEach((element) => result += _map[element]);
+  String toRna(String s) {
+    var result = '';
+    for (var i = 0; i < s.length; i++) {
+      result += dnaConverter(s[i]);
+    }
     return result;
   }
+}
+
+String dnaConverter(String dna) {
+  String rna;
+  if (dna == 'G') {
+    rna = 'C';
+  } else if (dna == 'C') {
+    rna = 'G';
+  } else if (dna == 'T') {
+    rna = 'A';
+  } else if (dna == 'A') {
+    rna = 'U';
+  }
+  return rna;
 }
